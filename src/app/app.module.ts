fcc10fa4c32cd671542fee73773f66a9b964085c
coaltail <coaltail420@gmail.com>
2022-03-29 11:11:15 +0200

    added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProjectsComponent } from './components/projects/projects.component';
 
 import { SidebarModule } from '@syncfusion/ej2-angular-navigations';
 import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { HomeComponent } from './components/home/home.component';
     AboutComponent,
     ContactComponent,
     ProjectsComponent,
-    HomeComponent
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
