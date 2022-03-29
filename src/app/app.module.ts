93b2e33187a7ce008a27b65bdf8ed3103b487d5f
coaltail <coaltail420@gmail.com>
2022-03-29 16:01:27 +0200

    Completed home page, fixed header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { HeaderComponent } from './components/header/header.component';
-import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 
@@ -19,7 +18,6 @@ import { FooterComponent } from './components/footer/footer.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    AboutComponent,
     ContactComponent,
     ProjectsComponent,
     HomeComponent,
